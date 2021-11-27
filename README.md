cd5d658fec782795712ac5c83d78b33e78d730a1
Joel Zamboni <joel.zamboni@webera.com>
2021-11-26 20:15:44 -0500

    fix issues and removing fancy login

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Dev and Ops Tools
-Collection of scripts and tools to improve dev and ops workflows
 
-# Tools
+Collection of scripts and tools to improve dev and ops workflows. Besides the
+installation it also configures the `.bashrc` file with a helper script which
+loads all the autocomplete for the installed tools.
 
-## 
+## Installed Tools
+* GitLab Runnner
+* Helm
+* Kustomize 
+* Kubectl
+* Skaffold
+* Kind
+* Tekton CLI
+* eksctl
+* Terraform
+* gcloud
+* AWS CLI v2
+* Flux CLI
+* ArgoCD CLI
--- essentials.sh
+++ essentials.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 [ "${BASH_VERSINFO:-0}" -ge 4 ] || { echo "Upgrade your bash to at least version 4"; exit 1; }
 
-# Script create to be self-contained and install several basic utilities. Works on Ubuntu and its docker images
-set -e
+set -eux
 umask 022
-export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/sbin:
-
-# Some emojis
-OK='echo -e \xE2\x9C\x94'
-NOK='echo -e \xE2\x9C\x96'
-BEER='\xF0\x9F\x8D\xBA'
-TEACUP='\xF0\x9F\x8D\xB5'
-GREEN="\e[32m"
-RED="\e[31m"
-NORMAL="\e[0m"
-
 
 # Configuration definitions
-BIN_DIR=${HOME}/bin
-TMP_DIR=${HOME}/tmp/workstation-tmp
-LOG_FILE="/dev/null"
+export BIN_DIR=${HOME}/bin
+export TMP_DIR=${HOME}/tmp/workstation-tmp
+export PATH=${BIN_DIR}:/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/sbin:
 UBUNTU_PACKAGES=(
   apt-utils
   docker.io
@@ -40,21 +28,7 @@ UBUNTU_PACKAGES=(
 mkdir -p ${BIN_DIR} ${TMP_DIR}
 cd ${TMP_DIR}
 
-function echoRed() {
-  echo -e "${RED}${1}${NORMAL}"
-}
-
-function echoGreen() {
-  echo -e "${GREEN}${1}${NORMAL}"
-}
-
 # All functions, I know they should be in a separated file, but they are here to keep all in one file 
-# Receives binary name and url
-function simple_install() {
-  curl -sL --output ${BIN_DIR}/$1 $2
-  chmod +x ${BIN_DIR}/$1
-}
-
 function ubuntu_packages()
 {
   SUDO=''
@@ -71,15 +45,16 @@ function ubuntu_packages()
 
 function install_gitlab_runner()
 {
-  simple_install gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
+  curl -sL --output ${BIN_DIR}/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
+  chmod +x ${BIN_DIR}/gitlab-runner
 }
 
 function install_helm()
 {
-  LATEST_V3=$(curl -Ls https://github.com/helm/helm/releases | grep 'href="/helm/helm/releases/tag/v3.[0-9]*.[0-9]*\"' | grep -v no-underline | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
-  curl -sLO https://get.helm.sh/helm-${LATEST_V3}-linux-amd64.tar.gz 
-  tar zxvf helm-${LATEST_V3}-linux-amd64.tar.gz
-  mv linux-amd64/helm ${BIN_DIR}
+  export USE_SUDO="false"
+  export HELM_INSTALL_DIR="${BIN_DIR}"
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  bash -x get_helm.sh
 }
 
 function install_kustomize()
@@ -90,21 +65,24 @@ function install_kustomize()
 
 function install_skaffold()
 {
-  simple_install skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+  curl -sL --output ${BIN_DIR}/skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+  chmod +x ${BIN_DIR}/skaffold
 }
 
 function install_kubectl() 
 {
   VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
   URL="https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kubectl"
-  simple_install kubectl ${URL}
+  curl -sL --output ${BIN_DIR}/kubectl ${URL}
+  chmod +x ${BIN_DIR}/kubectl
 }
 
 function install_kind()
 {
   VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | grep tag_name | cut -d'"' -f4)
   URL="https://github.com/kubernetes-sigs/kind/releases/download/${VERSION}/kind-linux-amd64"
-  simple_install kind ${URL}
+  curl -sL --output ${BIN_DIR}/kind ${URL}
+  chmod +x ${BIN_DIR}/kind
 }
 
 function install_tekton_cli()
@@ -112,6 +90,7 @@ function install_tekton_cli()
   VERSION=$(curl -s https://api.github.com/repos/tektoncd/cli/releases/latest | grep tag_name | cut -d'"' -f4)
   curl -sLO https://github.com/tektoncd/cli/releases/download/${VERSION}/tkn_${VERSION#?}_Linux_x86_64.tar.gz
   tar zxvf tkn_${VERSION#?}_Linux_x86_64.tar.gz -C ${BIN_DIR} tkn
+  [ -x ${BIN_DIR}/tkn ] || { echo "Error on tekton"; exit 1; }
 }
 
 function install_eksctl()
@@ -131,16 +110,21 @@ function install_terraform()
 
 function install_gcloud()
 {
+  [ -d ${HOME}/opt/google-cloud-sdk ] && return
+  mkdir -p ${HOME}/opt
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-  export CLOUDSDK_INSTALL_DIR=${BIN_DIR}
+  export CLOUDSDK_INSTALL_DIR=${HOME}/opt
   curl https://sdk.cloud.google.com | bash 
+  ${HOME}/opt/google-cloud-sdk/install.sh --quiet --bash-completion true
 }
 
 function install_aws()
 {
+  [ -d ${HOME}/opt/aws-cli ] && return
+  mkdir -p ${HOME}/opt
   curl -sL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip 
-  aws/install -i ${BIN_DIR}/aws-cli -b ${BIN_DIR}
+  aws/install -i ${HOME}/opt/aws-cli -b ${BIN_DIR}
 }
 
 function install_flux()
@@ -152,13 +136,8 @@ function install_flux()
 function install_argocd()
 {
   VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
-  curl --silent --location -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
-  chmod +x /usr/local/bin/argocd
-}
-
-function install_k9s()
-{
-  snap install k9s
+  curl -sLo ${BIN_DIR}/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+  chmod +x ${BIN_DIR}/argocd
 }
 
 function configure_helper() 
@@ -175,6 +154,7 @@ source <(tkn completion bash)
 source <(eksctl completion bash)
 source <(flux completion bash)
 source <(argocd completion bash)
+complete -C \${HOME}/bin/aws_completer aws
 umask 022
 export EDITOR=vim
 EOL
@@ -183,30 +163,28 @@ EOL
 }
 
 
-echo -e "- Installing tools, packages, and updates. Time for a ${BEER} or a ${TEACUP}"
-declare -A STEPS=(
-  [install_kustomize]="Kustomize"
-  [install_gitlab_runner]="GitLab Runner"
-  [install_helm]="Helm"
-  [install_skaffold]="Scaffold"
-  [install_kubectl]="Kubectl"
-  [install_kind]="Kind"
-  [install_tekton_cli]="Tekton CLI"
-  [install_eksctl]="Eksctl"
-  [install_terraform]="Terraform"
-  [install_gcloud]="Google Cloud CLI"
-  [install_aws]="AWS CLI V2"
-  [install_flux]="Flux CLI"
-  [install_argocd]="ArgoCD CLI"
-  [install_k9s]="k9s CLI"
-  [configure_helper]="Shell Helper"
+declare STEPS=(
+  ubuntu_packages
+  install_kustomize
+  install_gitlab_runner
+  install_helm
+  install_skaffold
+  install_kubectl
+  install_kind
+  install_tekton_cli
+  install_eksctl
+  install_terraform
+  install_gcloud
+  install_aws
+  install_flux
+  install_argocd
+  configure_helper
 )
 
-printf '  - %-30s' "Ubuntu Packages" && ubuntu_packages >${LOG_FILE} 2>&1 && ${OK} || ${NOK}
 
-for STEP in "${!STEPS[@]}"
+for STEP in "${STEPS[@]}"
 do
-  printf '  - %-30s' "${STEPS[$STEP]}" && ${STEP} >${LOG_FILE} 2>&1 && ${OK} || ${NOK}
+  ${STEP} 
 done
 
 cd ${HOME}
